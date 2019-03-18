e4e1cc2e82808dae0a38b5e6bdcc5a0e845f164b
chenweiguang <weiguangchen@sf-express.com>
2019-03-18 10:11:31 +0800

    修改为父子项目依赖

---
+++ common/src/main/java/com/okayjam/bigdata/App.java
@@ -0,0 +1,24 @@
+package com.okayjam.bigdata;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger LOG =  LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        LOG.info("Current Time: {}", System.currentTimeMillis());
+        LOG.info("Curren Time: " + System.currentTimeMillis());
+        LOG.info("Current Time: {}", System.currentTimeMillis());
+        LOG.trace("trace log");
+        LOG.warn("warn log");
+        LOG.debug("debug log");
+        LOG.info("info log");
+    }
+}
