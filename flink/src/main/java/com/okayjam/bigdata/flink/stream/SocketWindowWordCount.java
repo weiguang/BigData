b947ecf0a97d35b8b55ad474325aa75c50255a2f
jamchen <jamchen@tencent.com>
2020-09-16 20:04:41 +0800

    Update SocketWindowWordCount.java
    flink demo update

--- flink/src/main/java/com/okayjam/bigdata/flink/stream/SocketWindowWordCount.java
+++ flink/src/main/java/com/okayjam/bigdata/flink/stream/SocketWindowWordCount.java
@@ -5,6 +5,9 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 /**
  * you need run `nc -lk hostname port` before run this demo
@@ -27,10 +30,13 @@ public class SocketWindowWordCount {
     SingleOutputStreamOperator<Tuple2<String, Integer>> sum = stream.flatMap(new LineSplitter())
            // .keyBy(0)
             .keyBy((value)->value.getField(0))
+            .timeWindow(Time.seconds(5), Time.seconds(3))
             .sum(1);
 
         // 将结果打印到控制台，注意这里使用的是单线程打印，而非多线程
-        sum.print().setParallelism(1);
+        //sum.print().setParallelism(1);
+        // sink 输出
+        sum.addSink(new PrintSinkFunction<>());
 
         env.execute("Java WordCount from SocketTextStream Example");
     }
