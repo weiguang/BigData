8610a34d27725343e4f87aebe14db5b7b1702143
jamchen <jamchen@tencent.com>
2020-09-16 20:04:16 +0800

    kafka demo  update

--- kafka/src/main/java/com/okayjam/bigdata/kafka/ConsumerDemo.java
+++ kafka/src/main/java/com/okayjam/bigdata/kafka/ConsumerDemo.java
@@ -4,6 +4,7 @@ package com.okayjam.bigdata.kafka;
  * @author: Chen weiguang <chen2621978@gmail.com>
  * @create: 2019/02/18 18:31
  **/
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -36,7 +37,7 @@ public class ConsumerDemo {
         consumer.subscribe(Arrays.asList(topicName));
         try {
             while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(1000);
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
                 for (ConsumerRecord<String, String> record : records) {
                     System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
                 }
