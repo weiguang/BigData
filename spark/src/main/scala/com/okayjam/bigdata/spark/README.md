98fd1dde3a471393f2052c2c697660035a2b577f
weiguang <chen2621978@gmail.com>
2022-11-25 11:08:23 +0800

    add spark kafka wordcount demo

---
+++ spark/src/main/scala/com/okayjam/bigdata/spark/README.md
@@ -0,0 +1,11 @@
+
+## windows run configure
+
+### 1. 需要下载hadoop 环境
+Download url：
+https://objects.githubusercontent.com/github-production-release-asset-2e65be/42450016/07bab578-8cd6-11e7-8682-592ce34f6978?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20221124%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221124T024036Z&X-Amz-Expires=300&X-Amz-Signature=8c8cb1657849517bbb48e751272a4e1a5d51fee252465dfd3f79e367a295097c&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=42450016&response-content-disposition=attachment%3B%20filename%3Dhadoop-2.8.1.zip&response-content-type=application%2Foctet-stream
+
+### 2. 下载解压之后，这是 HADOOP_HOME 和设置path为解压目录
+
+
+查看运行情况：http://localhost:4040
---
+++ spark/src/main/scala/com/okayjam/bigdata/spark/stream/KafkaWordCount.scala
@@ -0,0 +1,55 @@
+package com.okayjam.bigdata.spark.stream
+
+/**
+ * windows need download hadoop tool
+ *
+ * @author Chen weiguang chen2621978@gmail.com 
+ * @date 2022/11/24 10:18
+ * */
+
+import org.apache.spark._
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+object KafkaWordCount{
+  def main(args:Array[String]){
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]")
+    val sc = new SparkContext(sparkConf)
+    sc.setLogLevel("WARN")
+    val ssc = new StreamingContext(sc,Seconds(10))
+    //设置检查点，如果存放在HDFS上面，则写成类似ssc.checkpoint("/user/hadoop/checkpoint")这种形式，但是，要启动Hadoop
+    ssc.checkpoint("./spark/kafka/checkpoint")
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "9.135.90.x:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "jam_spark_stream",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (true: java.lang.Boolean)
+    )
+    val topics = Array("mykafka")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+    stream.foreachRDD(rdd => {
+      val offsetRange = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      val maped: RDD[(String, String)] = rdd.map(record => (record.key,record.value))
+      val lines = maped.map(_._2)
+      val words = lines.flatMap(_.split(" "))
+      val pair = words.map(x => (x,1))
+      val wordCounts = pair.reduceByKey(_+_)
+      wordCounts.foreach(println)
+//      wordCounts.collect().foreach(println)
+    })
+    ssc.start
+    ssc.awaitTermination
+  }
+}
